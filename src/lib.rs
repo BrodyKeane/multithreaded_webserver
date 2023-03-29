b6c6e7fd1d169cfeab52ce43dd6ecdb040a2149a
BrodyKeane <keanebrody8585@gmail.com>
2023-03-28 20:17:34 -0400

    multithreaded receivers are functional

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,54 @@
-use std::thread::{Builder, JoinHandle};
-use std::io;
+use std::{
+    thread::{Builder, JoinHandle},
+    sync::{mpsc, Arc, Mutex},
+    io,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
-pub struct Worker {
-    id: usize,
-    thread: JoinHandle<()>,
-}
+type Job = Box<dyn FnOnce() + Send + 'static>; 
 
-impl Worker {
-    fn new(id: usize) -> io::Result<Worker>{
-        let builder = Builder::new();
-        let thread = builder.spawn(|| {})?; 
-        Ok(Worker{ id, thread })
-    }
-}
-
-impl ThreadPool {
+impl ThreadPool { 
     pub fn new(size: usize) -> ThreadPool {
+        let (sender, receiver) = mpsc::channel(); 
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
-        
         for id in 0..size {
-            if let Ok(worker) = Worker::new(id) {
+            if let Ok(worker) = Worker::new(id, Arc::clone(&receiver)) {
                 workers.push(worker);
             }
         }
-
         assert!(workers.len() > 0);
-        ThreadPool{ workers }
+        ThreadPool{ workers, sender }
     }
+
 
     pub fn execute<F> (&self, f: F)
     where 
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> io::Result<Worker>{
+        let builder = Builder::new();
+        let thread = builder.spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker: {id} got a job; executing.");
+            job();
+        })?; 
+        
+        Ok(Worker{ id, thread })
     }
 }
